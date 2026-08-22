f4ec19c7861def056dd6284873af8f85c1023679
Sreyas Sunmesh <sunmeshesreyas@gmail.com>
2026-08-23 02:07:30 +0530

    Revert "Merge remote-tracking branch 'origin/master' into branchSreyas"
    This reverts commit 50f9e7f38e6f5f9c8d626a83ff249b2572738bf7, reversing
    changes made to b2a06bd4443cc27a637473365fd349b8557b3261.

---
+++ Muse.Api/Models/Artist.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Muse.Api.Models
+{
+    public class Artist
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required, MaxLength(150)]
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<Song> Songs { get; set; } = new List<Song>();
+        public ICollection<UserPreference> UserPreferences { get; set; } = new List<UserPreference>();
+
+  
+    }
+}
